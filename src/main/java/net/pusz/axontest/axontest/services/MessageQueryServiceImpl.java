e1b1691405c61d9ee7b7c52350dca7b644c2df31
Szymon Pusz <szymon@pusz.net>
2019-11-29 22:03:02 +0100

    Distributed command bus not working yet

--- src/main/java/net/pusz/axontest/axontest/services/MessageQueryServiceImpl.java
+++ src/main/java/net/pusz/axontest/axontest/services/MessageQueryServiceImpl.java
@@ -1,21 +1,22 @@
-package net.pusz.axontest.axontest.services;
+// package net.pusz.axontest.axontest.services;
 
-import java.util.List;
-import java.util.stream.Collectors;
+// import java.util.List;
+// import java.util.stream.Collectors;
 
-import org.axonframework.eventsourcing.eventstore.EventStore;
-import org.springframework.stereotype.Service;
+// import org.axonframework.eventsourcing.eventstore.EventStore;
+// import org.springframework.stereotype.Service;
 
-@Service
-public class MessageQueryServiceImpl implements MessageQueryService {
-    private final EventStore eventStore;
+// @Service
+// public class MessageQueryServiceImpl implements MessageQueryService {
+// private final EventStore eventStore;
 
-    public MessageQueryServiceImpl(EventStore eventStore) {
-        this.eventStore = eventStore;
-    }
+// public MessageQueryServiceImpl(EventStore eventStore) {
+// this.eventStore = eventStore;
+// }
 
-    @Override
-    public List<Object> listEventsForMessage(String id) {
-        return this.eventStore.readEvents(id).asStream().map(s -> s.getPayload()).collect(Collectors.toList());
-    }
-}
+// @Override
+// public List<Object> listEventsForMessage(String id) {
+// return this.eventStore.readEvents(id).asStream().map(s ->
+// s.getPayload()).collect(Collectors.toList());
+// }
+// }
