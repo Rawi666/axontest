6a20c74c159f5a1a38dd082835bc9355d1650e95
Szymon Pusz <szymon@pusz.net>
2019-11-30 14:46:32 +0100

    postgres added to docker-compose

--- src/test/java/net/pusz/axontest/axontest/DemoApplicationTests.java
+++ src/test/java/net/pusz/axontest/axontest/DemoApplicationTests.java
@@ -1,13 +1,13 @@
-package net.pusz.axontest.axontest;
+// package net.pusz.axontest.axontest;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class ProgramTests {
+// @SpringBootTest
+// class ProgramTests {
 
-	@Test
-	void contextLoads() {
-	}
+// @Test
+// void contextLoads() {
+// }
 
-}
+// }
