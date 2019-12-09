0e3c283e810ce7181acdcd621ea8c44af7fcce18
Pusz, Szymon <szymon.pusz@gft.com>
2019-12-09 16:41:15 +0100

    start and stop scripts fixed

--- k8s/clean_k8s.sh
+++ k8s/clean_k8s.sh
@@ -4,3 +4,4 @@ kubectl delete ClusterRoleBinding jgroups-kubeping-api-access
 kubectl delete ClusterRole jgroups-kubeping-pod-reader
 kubectl delete ServiceAccount jgroups-kubeping-service-account
 kubectl delete deployment postgresaxon
+kubectl delete service postgresaxon
--- k8s/start_k8s.sh
+++ k8s/start_k8s.sh
@@ -1,5 +1,7 @@
 kubectl apply -f ./clusterrole.yaml
 kubectl apply -f ./postgresaxon-deployment.yaml
 kubectl apply -f ./postgresaxon-service.yaml
+echo Waiting 10s before deploying axon
+sleep 10
 kubectl apply -f ./axonconsumer-deployment.yaml
 kubectl apply -f ./axonproducer-deployment.yaml
